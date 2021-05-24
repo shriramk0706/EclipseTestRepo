e456ab8315f5a69326f774b052e93d794381f9b9
Shriram Katla <shriramk0@gmail.com>
2021-05-25 01:17:51 +0530

    test2

---
+++ src/main/java/jenkinsTestProj/NumberClass.java
@@ -0,0 +1,19 @@
+package jenkinsTestProj;
+
+public class NumberClass {
+	
+	public NumberClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int addNumber(int x, int y) {
+		return x+y;
+	}
+	
+	public int substractNumber(int x, int y) {
+		return x-y;
+	}
+
+	
+
+}
---
+++ src/test/java/jenkinsTestProj/TestRunner.java
@@ -0,0 +1,23 @@
+package jenkinsTestProj;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRunner {
+
+	@Test
+	public void addmethod() {
+		NumberClass num = new NumberClass();
+		int result1 = num.addNumber(10, 5);
+		assertEquals(15, result1);
+	}
+	
+	@Test
+	public void substractmethod() {
+		NumberClass num2 = new NumberClass();
+		int result2 = num2.substractNumber(10, 5);
+		assertEquals(5, result2);
+	}
+
+}
